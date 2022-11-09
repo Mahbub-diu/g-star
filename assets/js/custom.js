03dcd8c596aa608634a21668f4e1c7f36349508f
bmnazmussakib <nazmussakib161@gmail.com>
2022-11-09 19:01:10 +0600

    carousel select size offcanvas

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,50 @@ $('.you-may-like-slider-wrapper').slick({
     infinite: false,
     speed: 300,
     slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay:true,
+    prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+  $('.explorer-slider-wrapper').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
     slidesToScroll: 4,
-    autoplay:true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
     responsive: [
       {
         breakpoint: 1024,
