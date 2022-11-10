39318df7712a653f0b68e0b50e9a9ac706817750
bmnazmussakib <nazmussakib161@gmail.com>
2022-11-10 11:38:55 +0600

    font added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,9 +32,6 @@ $('.you-may-like-slider-wrapper').slick({
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
 
