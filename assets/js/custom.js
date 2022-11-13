21e101ead90b7762a6db1e2b44116ea2a0801397
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-11-13 15:21:13 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,88 +1,124 @@
 $('.you-may-like-slider-wrapper').slick({
-    dots: false,
-    arrows: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    // autoplay:true,
-    prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
-    nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
+  dots: false,
+  arrows: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  // autoplay:true,
+  prevArrow:
+    '<span class="arrow_Prev"><i class="fas fa-angle-left"></i></span> ',
+  nextArrow:
+    '<span class="arrow_Next"><i class="fas fa-angle-right"></i></span>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
 
-
-  $('.explorer-slider-wrapper').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
+$('.explorer-slider-wrapper').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplay: true,
   autoplaySpeed: 2000,
   infinite: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
+  prevArrow:
+    '<span class="arrow_Prev"><i class="fas fa-angle-left"></i></span> ',
+  nextArrow:
+    '<span class="arrow_Next"><i class="fas fa-angle-right"></i></span>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
 
-  window.addEventListener('scroll', function() {
-    if (document.documentElement.scrollTop > 200) {
-      document.getElementById('header').classList.add('fixed-top')
-      document.getElementById('header').style.backgroundColor = '#fff'
-      document.querySelectorAll('.nav-link').classList.add('color-black')
-    }
-    if (document.documentElement.scrollTop < 200) {
-      document.getElementById('header').classList.remove('fixed-top')
-      document.getElementById('header').style.backgroundColor = 'transparent'
-    }
+window.addEventListener('scroll', function () {
+  if (document.documentElement.scrollTop > 200) {
+    document.getElementById('header').classList.add('fixed-top');
+    document.getElementById('header').style.backgroundColor = '#fff';
+    document.querySelectorAll('.nav-link').classList.add('color-black');
+  }
+  if (document.documentElement.scrollTop < 200) {
+    document.getElementById('header').classList.remove('fixed-top');
+    document.getElementById('header').style.backgroundColor = 'transparent';
+  }
+});
+
+$('.community-slider').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 6,
+  arrows: true,
+  prevArrow:
+    '<span class="arrow_Prev"><i class="fas fa-angle-left"></i></span> ',
+  nextArrow:
+    '<span class="arrow_Next"><i class="fas fa-angle-right"></i></span>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
