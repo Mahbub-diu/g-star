32478e2a40f19336b23428a21dedac6dc151a9d9
bmnazmussakib <nazmussakib161@gmail.com>
2022-11-17 18:51:51 +0600

    klothen homepage responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,7 +42,7 @@ $('.explorer-slider-wrapper').slick({
   infinite: false,
   speed: 300,
   slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   infinite: true,
